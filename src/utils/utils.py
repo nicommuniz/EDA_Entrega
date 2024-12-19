c009baadfdfa6a2ace53ee07133d443b72e07a65
nicommuniz <nicolasmmuniz@gmail.com>
2024-12-19 09:43:18 -0300

    EDA_ENTREGA

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -274,3 +274,22 @@ def bubble_plot(df, col_x, col_y, col_size, scale = 1000):
     plt.show()
 
 
+# Analisis para definir si es categorica o numerica.
+
+def card_tipo(df,umbral_categoria = 10, umbral_continua = 30):
+    # Primera parte: Preparo el dataset con cardinalidades, % variación cardinalidad, y tipos
+    df_temp = pd.DataFrame([df.nunique(), df.nunique()/len(df) * 100, df.dtypes]) # Cardinaliad y porcentaje de variación de cardinalidad
+    df_temp = df_temp.T # Como nos da los valores de las columnas en columnas, y quiero que estas sean filas, la traspongo
+    df_temp = df_temp.rename(columns = {0: "Card", 1: "%_Card", 2: "Tipo"}) # Cambio el nombre de la transposición anterior para que tengan más sentido, y uso asignación en vez de inplace = True (esto es arbitrario para el tamaño de este dataset)
+    
+    # Corrección para cuando solo tengo un valor
+    df_temp.loc[df_temp.Card == 1, "%_Card"] = 0.00
+    
+    # Creo la columna de sugerenica de tipo de variable, empiezo considerando todas categóricas pero podría haber empezado por cualquiera, siempre que adapte los filtros siguientes de forma correspondiente
+    df_temp["tipo_sugerido"] = "Categorica"
+    df_temp.loc[df_temp["Card"] == 2, "tipo_sugerido"] = "Binaria"
+    df_temp.loc[df_temp["Card"] >= umbral_categoria, "tipo_sugerido"] = "Numerica discreta"
+    df_temp.loc[df_temp["%_Card"] >= umbral_continua, "tipo_sugerido"] = "Numerica continua"
+    # Ojo los filtros aplicados cumplen con el enunciado pero no siguen su orden y planteamiento
+
+    return df_temp
